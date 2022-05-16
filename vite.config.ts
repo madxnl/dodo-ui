ca12b64903403affdcc3ca79a44c219b38f0d1a7
Doeke de Wolf <doekedewolf@gmail.com>
2022-05-16 15:35:23 +0200

    setup for library export

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+// See:
+// https://vitejs.dev/guide/build.html#library-mode
+// https://jivancic.com/posts/build-a-component-library.html#vite-config
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [vue()]
+// })
+
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Dodo-UI',
+      fileName: (format) => `dodo-ui.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
