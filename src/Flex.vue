7af4b4a2f60ffec668c53f756eabaa2b503ad9a0
Doeke de Wolf <doekedewolf@gmail.com>
2022-05-17 01:07:02 +0200

    storybook+vue3+ts errors

---
+++ src/Flex.vue
@@ -0,0 +1,15 @@
+
+<template>
+  <div :style="css"><slot></slot></div>
+</template>
+<script lang="ts" setup>
+  import { computed } from "vue";
+
+const props = defineProps<{
+  gap: number,
+}>()
+
+const css = computed(() => {
+  return `gap:${props.gap}`
+})
+</script>
