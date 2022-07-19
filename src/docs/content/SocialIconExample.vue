7f10ac28e625eafed69a6626d34e053e461a0fa3
Doeke de Wolf <doekedewolf@gmail.com>
2022-07-19 13:00:27 +0200

    brand logos

---
+++ src/docs/content/SocialIconExample.vue
@@ -0,0 +1,18 @@
+<template>
+  <Row>
+    <SocialIcon name="google" large />
+    <SocialIcon name="twitter" large />
+    <SocialIcon name="slack" large />
+    <SocialIcon name="facebook" large />
+    <SocialIcon name="linkedin" large />
+    <SocialIcon name="whatsapp" large />
+  </Row>
+  <Row style="background:silver" pad="s">
+    <SocialIcon name="google" white large />
+    <SocialIcon name="twitter" white large />
+    <SocialIcon name="slack" white large />
+  </Row>
+</template>
+<script lang="ts" setup>
+import { Row, SocialIcon } from '../..'
+</script>
